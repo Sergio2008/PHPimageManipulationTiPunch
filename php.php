f45ed40f17126ae0a5980685dd73113d0acf5e17
Antoine SESTIER <asestier@ecedi.fr>
2017-06-19 09:35:44 +0200

    add scale for motif

--- php.php
+++ php.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 	/* ------- reglages des parametre ------ */
 
 	$transparency = 100;
@@ -7,16 +7,16 @@
 
 
 	// On charge d'abord les images
-	
+
 
 	$uploadScFile = "images/motif/";
-	$sourceName ="ecusson2.png"; 
-	$pictureSourceUrl = $uploadScFile  . $sourceName;
+	$sourceName ="ecusson.png";
+	$pictureSourceUrl = $uploadScFile . $sourceName;
 	// echo $pictureSourceUrl;
 
 	$uploadDsFile = "images/support/";
-	$destinationName ="hommeTshirt.png"; 
-	$pictureDestinationUrl = $uploadDsFile  . $destinationName;
+	$destinationName ="hommeTshirt.png";
+	$pictureDestinationUrl = $uploadDsFile . $destinationName;
 	// echo $pictureDestinationUrl;
 
 
@@ -26,46 +26,68 @@
 
         // copying relevant section from background to the cut resource
         imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h);
-       
+
         // copying relevant section from watermark to the cut resource
         imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h);
-       
+
         // insert cut resource to destination image
         imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct);
-    } 
+    }
 
 	$source = imagecreatefrompng($pictureSourceUrl); // Le logo est la source
-
-	$destination = imagecreatefrompng($pictureDestinationUrl); // La photo est la destination
-
-
-	// Les fonctions imagesx et imagesy renvoient la largeur et la hauteur d'une image
 
 	$largeur_source = imagesx($source);
 
 	$hauteur_source = imagesy($source);
+	// MISE A 100% DE LARGEUR OU HAUTEUR (SELON LE MOTIF) DE LA ZONE UTILE
+	
+	$largeur_max = 1181;
+	$hauteur_max = 2362;	
+	
+	// Étape 1 :
+	$NouvelleLargeur = 350;
+	
+	// Étape 2 :
+	$Reduction = ( ($NouvelleLargeur * 100)/$largeur_source );
+	
+	// Étape 3 :
+	$NouvelleHauteur = ( ($hauteur_source * $Reduction)/100 );
+
+	$sourceScaled = imagescale( $source, $NouvelleLargeur, $NouvelleHauteur );
+	//imagepng($source, $pictureSourceUrl);
+
+	echo ("La nouvelle largeur du motif est de " . $NouvelleLargeur . " px. ");
+	//
+	$destination = imagecreatefrompng($pictureDestinationUrl); // La photo est la destination
+	echo (getimagesize($source));
+
+
+	// Les fonctions imagesx et imagesy renvoient la largeur et la hauteur d'une image
 
 	$largeur_destination = imagesx($destination);
 
 	$hauteur_destination = imagesy($destination);
 
 
-	// On veut placer le logo en bas à droite, on calcule les coordonnées où on 	doit placer le logo sur la photo
-/*
-	$destination_x = $largeur_destination - $largeur_source;
+	// On veut placer le logo en haut à droite, on calcule les coordonnées où on 	doit placer le logo sur la photo
 
-	$destination_y =  $hauteur_destination - $hauteur_source;
-	*/
-	$destination_x = $_POST['x'];
+	//$destination_x = $largeur_destination - $NouvelleLargeur;
 
-	$destination_y =  $_POST['y'];
+	//$destination_y =  $hauteur_destination - $NouvelleHauteur;
 
+	$destination_x = $_POST['x']+673; // COIN HAUT GAUCHE DE LA ZONE UTILE EN X
 
+	$destination_y =  $_POST['y']+613; // COIN HAUT GAUCHE DE LA ZONE UTILE EN Y
+	
+// $destination_x = 0;
+// $destination_y = 0;
 	// On met le logo (source) dans l'image de destination (la photo)
+
 
 	imagesavealpha($source, true);
 	imagesavealpha($destination, true);
-	imagecopymerge_alpha($destination, $source, $destination_x, $destination_y, 0, 0, 	$largeur_source, $hauteur_source, $transparency);
+
+	imagecopymerge_alpha($destination, $sourceScaled, $destination_x, $destination_y, 0, 0, 	$NouvelleLargeur, $NouvelleHauteur, $transparency);
 
 
 	// On affiche l'image de destination qui a été fusionnée avec le logo
@@ -82,50 +104,20 @@
 	$resultName = "mon_image";
 	$newExtentionName = ".png";
 	$newFile = $storageFile.$resultName.$newExtentionName;
-	
+
 	/* ---- save the new file ----- */
 
 	imagesavealpha($destination, true);
 	imagesavealpha($source, true);
-    imagepng($destination, $newFile);
+  	imagepng($destination, $newFile);
 
     // echo $newFile;
-    
+echo "<br/><img src='$newFile' width='50%' height='auto'><br/>";
 
 	$arr = array('source' => $newFile, 'loading' => 'finished');
 	$arr2 = json_encode($arr);
 	echo $arr2;
-	//echo json_decode($arr2);
-	
+	// echo json_decode($arr2);
 
-	/*	$a = "dfwbsd";
-	var_dump ($a); 
-	*/
-/*
-<!DOCTYPE html>
-<html>
-<head>
-		<title>titre</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
 
-<body>
-		<form method="post" action="reception.php" enctype="multipart/form-data">
-     <label for="icone">Icône du fichier (JPG, PNG ou GIF | max. 15 Ko) :</label><br />
-     <input type="file" name="icone" id="icone" /><br />
-     <label for="mon_fichier">Fichier (tous formats | max. 1 Mo) :</label><br />
-     <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-     <input type="file" name="mon_fichier" id="mon_fichier" /><br />
-     <label for="titre">Titre du fichier (max. 50 caractères) :</label><br />
-     <input type="text" name="titre" value="Titre du fichier" id="titre" /><br />
-     <label for="description">Description de votre fichier (max. 255 caractères) :</label><br />
-     <textarea name="description" id="description"></textarea><br />
-     <input type="submit" name="submit" value="Envoyer" />
-</form>
-	</body>
-
-	
-</html>
-*/
 ?>
